8eaae68f54d37ff8c0d24185d22b767838fceebc
Sourabh S Nath <sourabhsnath@gmail.com>
2021-06-13 19:57:00 +0530

    Add sort and limit

--- app/src/main/java/dev/sourabh/programmerhumor/data/remote/RedditService.kt
+++ app/src/main/java/dev/sourabh/programmerhumor/data/remote/RedditService.kt
@@ -1,6 +1,8 @@
 package dev.sourabh.programmerhumor.data.remote
 
+import dev.sourabh.programmerhumor.data.response.RedditResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RedditService {
@@ -8,8 +10,10 @@ interface RedditService {
     /* raw_json for this reason:
      * https://stackoverflow.com/questions/41909161/reddit-api-how-do-i-convert-amp-to-in-angular-typescript-ionic
      */
-    @GET("r/ProgrammerHumor/top.json?raw_json=1")
+    @GET("r/ProgrammerHumor/{sort}.json?raw_json=1")
     suspend fun fetchProgrammerMemes(
-        @Query("after") after: String
-    )
+        @Path("sort") sort: String,
+        @Query("limit") limit: Int,
+        @Query("after") after: String?
+    ): RedditResponse
 }
